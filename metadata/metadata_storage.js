37f473a8c2c51054800a9d0e19e47d314bd38245
Shobit garg <122355051+shobit000@users.noreply.github.com>
2024-12-06 18:00:49 +0530

    Update metadata_storage.js

--- metadata/metadata_storage.js
+++ metadata/metadata_storage.js
@@ -1,1 +1,31 @@
+// metadata_storage.js
 
+class MetadataStorage {
+  constructor() {
+    this.metadata = {};
+  }
+
+  // Method to upload and store file metadata
+  uploadMetadata(fileId, metadata) {
+    this.metadata[fileId] = metadata;
+  }
+
+  // Method to retrieve file metadata
+  getMetadata(fileId) {
+    return this.metadata[fileId] || null;
+  }
+
+  // Method to update file metadata
+  updateMetadata(fileId, metadata) {
+    if (this.metadata[fileId]) {
+      this.metadata[fileId] = { ...this.metadata[fileId], ...metadata };
+    }
+  }
+
+  // Method to delete file metadata
+  deleteMetadata(fileId) {
+    delete this.metadata[fileId];
+  }
+}
+
+module.exports = MetadataStorage;
