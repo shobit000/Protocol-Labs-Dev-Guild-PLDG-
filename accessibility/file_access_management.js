3340b72e999cfe164558da20b0631eefc1bb5561
Shobit garg <122355051+shobit000@users.noreply.github.com>
2024-12-06 18:01:09 +0530

    Update file_access_management.js

--- accessibility/file_access_management.js
+++ accessibility/file_access_management.js
@@ -1,1 +1,29 @@
+// file_access_management.js
 
+class FileAccessManagement {
+  constructor() {
+    this.accessControlList = {};
+  }
+
+  // Method to grant access to a file
+  grantAccess(fileId, userId) {
+    if (!this.accessControlList[fileId]) {
+      this.accessControlList[fileId] = new Set();
+    }
+    this.accessControlList[fileId].add(userId);
+  }
+
+  // Method to revoke access to a file
+  revokeAccess(fileId, userId) {
+    if (this.accessControlList[fileId]) {
+      this.accessControlList[fileId].delete(userId);
+    }
+  }
+
+  // Method to check if a user has access to a file
+  hasAccess(fileId, userId) {
+    return this.accessControlList[fileId] && this.accessControlList[fileId].has(userId);
+  }
+}
+
+module.exports = FileAccessManagement;
